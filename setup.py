8e89c69355e2c8548c96c40068393464e6c6400c
arshigoyal12 <arshigoyal2002@gmail.com>
2023-12-28 14:57:48 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+# from typing import List
+
+def get_requirements(file_path:str):
+    ''' 
+    this function will return the list of
+    requirements from the requirements.txt file
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+    return requirements
+
+setup(
+name = "ml project",
+version = "0.0.1",
+author = "Arshi Goyal",
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
